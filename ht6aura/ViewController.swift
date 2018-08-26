510f287d869277d6b22e3f4edcf881b5ccfd5238
JeffTheHautbois <jeffpengyj@hotmail.com>
2018-08-25 21:38:48 -0400

    Initial Commit

---
+++ ht6aura/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ht6aura
+//
+//  Created by Jeff Peng on 2018-08-25.
+//  Copyright © 2018 Jeff Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
